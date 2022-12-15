2d62d44946883ff626da7b41f053d51a5239175d
Komi Serge Innocent [GEBEYA DMGP/DD/DPD] <sergekomi.innocent@orange-sonatel.com>
2022-12-15 19:25:35 +0000

    Enregistrer le fichier et appeler le service python pour notification.
    Externaliser le chemin du repertoire de sauvegarde
    
    Externaliser le endpoint du microservice python a notifier

--- src/main/java/com/stresstest/apirest_beac/controller/UploadFileController.java
+++ src/main/java/com/stresstest/apirest_beac/controller/UploadFileController.java
@@ -1,34 +1,24 @@
 package com.stresstest.apirest_beac.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.stresstest.apirest_beac.service.UploadFileService;
 
 @RestController
+@RequestMapping("/files")
+@RequiredArgsConstructor
 public class UploadFileController {
-	@Autowired
-	UploadFileService uploadFileService;
-	///{idBanque}
-	@PostMapping("/uploadFile")
-	public String submit(@RequestParam ("file") MultipartFile file) 
-				throws IllegalStateException, IOException {
-					 //TODO add try catch
-						// TODO Path_to_directory + idBanque recuprer la valeur du path_directory depuis le fichier properties.
-					uploadFileService.uploadFile(file);
-					//	file.transferTo(new File("C:\\Users\\ACER\\Documents\\FilesUploadedStressTest"+file.getOriginalFilename()+"idBanque"));
-					 //Files.copy(file.getInputStream(), "Path_to_directory", StandardCopyOption.REPLACE_EXISTING);
-					 //TODO notifier le service python du nouveau chargement de fichier.
-			 return "File has uploaded successfully";
-		}
+    private final UploadFileService uploadFileService;
 
-	}
+    @PostMapping("/uploadFile/{idBank}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public void submit(@RequestParam("file") MultipartFile file, @PathVariable String idBank) {
+        uploadFileService.uploadFileAndNotify(file, idBank);
+    }
+
+}
