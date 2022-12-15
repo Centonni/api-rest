258cf6aa30d3903f55c3a44c673914c8eabbab77
Komi Serge Innocent <komi.innocent@gmail.com>
2022-12-15 19:28:33 +0000

    Update UploadFileService.java

--- src/main/java/com/stresstest/apirest_beac/service/UploadFileService.java
+++ src/main/java/com/stresstest/apirest_beac/service/UploadFileService.java
@@ -2,18 +2,67 @@ package com.stresstest.apirest_beac.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
+import com.stresstest.apirest_beac.modele.Scenario;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 @Service
+@Slf4j
 public class UploadFileService {
-	@Value("file_destination")
-	String fileDestination;
+
+	private final String fileDestination;
+
+	private final String pythonServiceEndpoint;
+
+	private final RestTemplate restTemplate;
+
+	public UploadFileService(@Value("${beac.upload.dest.directory}") String fileDestination,
+							 @Value("${beac.upload.python.ms.endpoint}") String pythonServiceEndpoint,
+							 RestTemplateBuilder restTemplateBuilder) {
+		this.fileDestination = fileDestination;
+		this.pythonServiceEndpoint = pythonServiceEndpoint;
+		this.restTemplate=restTemplateBuilder.build();
+	}
+
 	public void uploadFile(MultipartFile file) throws IllegalStateException, IOException {
 		
 		file.transferTo(new File("C:\\FilesUploadedStressTest\\"+file.getOriginalFilename()));
+	}
+
+	public void uploadFileAndNotify(MultipartFile file,String idBank) {
+
+		try {
+
+			String fileName = "bank_"+idBank+"_"+ System.currentTimeMillis();
+			String destinationPath = fileDestination + fileName;
+			File fileSourcePath =  new File(destinationPath);
+			Files.copy(file.getInputStream(), fileSourcePath.toPath(),REPLACE_EXISTING);
+
+
+			var headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			var request =new HttpEntity(null, headers);
+
+			ResponseEntity<String> response = restTemplate.postForEntity(pythonServiceEndpoint, request, String.class);
+
+			log.info("response {}",response);
+
+		} catch (IOException e) {
+			log.error("Exception when saving files for bank {}",idBank,e);
+		}
 	}
 
 }
