c499179906afb1ec7213bb22cd107dbed46334ca
Komi Serge Innocent <komi.innocent@gmail.com>
2022-12-15 21:54:29 +0000

    Update ScenarioController.java

--- src/main/java/com/stresstest/apirest_beac/controller/ScenarioController.java
+++ src/main/java/com/stresstest/apirest_beac/controller/ScenarioController.java
@@ -6,14 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.stresstest.apirest_beac.exception.RessourceNotFoundException;
 import com.stresstest.apirest_beac.modele.Scenario;
@@ -22,6 +15,7 @@ import com.stresstest.apirest_beac.repository.ScenarioRepository;
 
 @RestController
 @RequestMapping("/scenario")
+@CrossOrigin
 public class ScenarioController {
 	
 	@Autowired
