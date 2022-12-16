3bd8b83b4886a482b06d6abf0deeab827436aacb
Komi Serge Innocent [GEBEYA DMGP/DD/DPD] <sergekomi.innocent@orange-sonatel.com>
2022-12-16 01:24:54 +0000

    polish

--- src/main/java/com/stresstest/apirest_beac/controller/BanqueController.java
+++ src/main/java/com/stresstest/apirest_beac/controller/BanqueController.java
@@ -2,10 +2,10 @@ package com.stresstest.apirest_beac.controller;
 
 import java.util.List;
 
+import com.stresstest.apirest_beac.modele.Scenario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.stresstest.apirest_beac.repository.BanqueRepository;
 import com.stresstest.apirest_beac.modele.Banque;
@@ -18,10 +18,20 @@ public class BanqueController {
 	@Autowired
 	private BanqueRepository banqueRepository;
 	
-	@RequestMapping("/banques")
+	@GetMapping("/banques")
 	public List<Banque> getAllBanques(){
 		return banqueRepository.findAll();
-		
+	}
+
+	@PostMapping("/banques-create")
+	public Banque createBanque(@RequestBody Banque scenario) {
+		return banqueRepository.save(scenario);
+	}
+
+	@DeleteMapping("/banques-delete/{id}")
+	public ResponseEntity<Void> createBanque(@PathVariable long id) {
+		 banqueRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 
 }
